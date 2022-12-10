5104eb69f21a557db72c28fbecf1e5a4fcbf171e
medaminehssini <medhssinihssini@gmail.com>
2022-12-10 19:43:05 +0100

    b

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import demandeRoutes from "./routes/demandeRoutes.js";
 connectDB();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
